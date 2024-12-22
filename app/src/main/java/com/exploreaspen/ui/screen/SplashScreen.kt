252ef6a05d9e06a1f9a6087589a32c077d890ca5
Wander Amaral <wanderamaral013@gmail.com>
2024-12-22 12:39:00 -0300

    Initial commit

---
+++ app/src/main/java/com/exploreaspen/ui/screen/SplashScreen.kt
@@ -0,0 +1,77 @@
+package com.exploreaspen.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.tooling.preview.Preview
+import com.exploreaspen.R
+import com.exploreaspen.ui.theme.GradientBlueTheme
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier) {
+
+    LaunchedEffect(key1 = Unit) {
+        delay(4_000)
+        // definir aqui a navegação desta tela
+    }
+
+
+    Box(
+        modifier = modifier
+            .background(GradientBlueTheme)
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_white),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .size(200.dp)
+            )
+            Spacer(modifier = Modifier.height(18.dp))
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(fontWeight = androidx.compose.ui.text.font.FontWeight.Bold)) {
+                        append("Health")
+                    }
+                    append("Track")
+                },
+                fontSize = 30.sp,
+                color = Color.White,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun SplashScreenPreview() {
+    SplashScreen()
+}
---
+++ app/src/main/java/com/exploreaspen/ui/theme/Theme.kt
@@ -0,0 +1,61 @@
+package com.exploreaspen.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+private val LightColors = lightColorScheme(
+    primary = GreenBase,
+    onPrimary = Color.White,
+    secondary = RedBase,
+    onSecondary = Color.White,
+    background = Gray100,
+    onBackground = Gray600,
+    surface = Gray100,
+    onSurface = Gray600,
+    error = RedBase,
+    onError = Color.White
+)
+
+private val DarkColors = darkColorScheme(
+    primary = GreenLight,
+    onPrimary = Color.Black,
+    secondary = RedLight,
+    onSecondary = Color.Black,
+    background = Gray600,
+    onBackground = Gray100,
+    surface = Gray600,
+    onSurface = Gray200,
+    error = RedBase,
+    onError = Color.Black
+)
+
+
+val GradientBlueTheme = Brush.linearGradient(
+    colors = listOf(
+        Color(0xFF33E4DB),
+        Color(0xFF00BBD3)
+    )
+)
+
+@Composable
+fun ExploreAspenTheme(
+    darkTheme: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColors
+    } else {
+        LightColors
+    }
+
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        content = content
+    )
+}
